e71bda6cb9411d114eb86aec73c2d5dc336b34d4
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-07-07 02:20:11 +0000

    Snapshot of branch master taken on 2019-07-06T22:20:06.929-04:00 from "https://github.com/UnofficialJuliaMirror/GeoStats.jl-dcc97b0b-8ce5-5539-9008-bb190f959ef6"

--- src/GeoStats.jl
+++ src/GeoStats.jl
@@ -5,41 +5,10 @@
 module GeoStats
 
 using Reexport
-using Random
-using LinearAlgebra
-using Distributed
-using StaticArrays
-using RecipesBase
-
-# reexport basic packages
-@reexport using Distances
-@reexport using Distributions
 
 # reexport project modules
 @reexport using GeoStatsBase
+@reexport using Variography
 @reexport using KrigingEstimators
 
-# extend base module
-import GeoStatsBase: solve, solve_single, preprocess
-
-# solvers installed by default
-include("solvers/kriging.jl")
-include("solvers/seqsim.jl")
-include("solvers/sgsim.jl")
-include("solvers/cookiecutter.jl")
-
-# solver comparisons
-include("comparisons/cross_validation.jl")
-include("comparisons/visual_comparison.jl")
-
-export
-  # solvers
-  Kriging,
-  SeqGaussSim,
-  CookieCutter,
-
-  # solver comparisons
-  VisualComparison,
-  CrossValidation
-
-end # module
+end
